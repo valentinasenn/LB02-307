6fbe1824ee67305c605ab7ff11b2749036ee8b67
valentinasenn <sennv@bzz.ch>
2022-05-31 20:38:05 +0200

    git init

--- js/validate-inputs.js
+++ js/validate-inputs.js
@@ -9,17 +9,21 @@ const name = document.getElementById('name');
 const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const repeatPassword = document.getElementById('passwordRepeat')
 const number = document.getElementById('number');
 const message = document.getElementById('message');
 const file = document.getElementById('file');
 const button = document.getElementById('button')
 
+function alertdialog() {
+    alert('Hallo')
+}
 
 // Show input error message
 function showError(input, message) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
     small.innerText = message;
 }
 
@@ -27,6 +31,24 @@ function showError(input, message) {
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
+}
+
+function checkSurname(input) {
+    const regex = /^[a-z ,.'-]+$/i;
+    if (regex.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Surname should not contain numbers')
+    }
+}
+
+function checkLastname(input) {
+    const regex = /^[a-z ,.'-]+$/i;
+    if (regex.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Lastname should not contain numbers')
+    }
 }
 
 // Check email is valid
@@ -39,18 +61,28 @@ function checkEmail(input) {
     }
 }
 
-/* Aufgabe:
-    Validieren Sie die Mobile-Nummer ähnlich wie bei der Email mit einer
-    Regular expression (regex). Für eine geeignete regex suchen Sie
-    im Internet nach "javascript regular expression for mobile number".
-*/
-// Check phone is valid
+function checkTel(input) {
+    var regex = /^\d{10}$/;
+    if (regex.text(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Telephone Number is not valid');
+    }
+}
 
+
+function matchPassword(input, repeatInput) {
+    if (input === repeatInput) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Passwords don\'t match');
+    }
+}
 
 // Check required fields
 function checkRequired(inputArr) {
     let isRequired = false;
-    inputArr.forEach(function(input) {
+    inputArr.forEach(function (input) {
         if (input.value.trim() === '') {
             showError(input, `${getFieldName(input)} is required`);
             isRequired = true;
@@ -79,38 +111,26 @@ function checkLength(input, min, max) {
     }
 }
 
-/* Aufgabe:
-        Validieren Sie, ob die beiden Passwörter übereinstimmen.
-        Falls sie nicht übereinstimmen, geben Sie (ähnlich wie in den anderen Beispielen)
-        eine Fehlermeldung dem Formular aus.
-*/
-// Check passwords match
-
 // Get fieldname
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-function validateForm(){
-    if(!checkRequired([name, email, password])){
-        //Aufgabe: Validierung der Länge für Vorname (2 bis 20) und Nachname (2 bis 50)
+function validateForm() {
+    if (!checkRequired([name, lastname, email, password, repeatPassword, message])) {
         checkLength(name, 3, 15);
-        checkLength(password, 6, 25);
-        /* Aufgabe:
-          Validierung der Telefonnumer ähnlich wie bei der Email mit einer
-          Regular expression (regex). Für eine geeignete regex suchen Sie
-          im Internet nach "javascript regular expression for mobile number"
-        * */
+        checkLength(lastname, 2, 50)
+        checkLength(password, 6, 10);
         checkEmail(email);
-        /* Aufgabe:
-          Validierung Sie die beiden Passwörter, damit password
-          mit password2 übereinstimmt.
-        * */
-    }
+        checkTel(number);
+        matchPassword(password, repeatPassword);
+        checkLastname(lastname)
+        checkSurname(name);
+  // }
 }
 
 // Event listeners
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     //https://www.w3schools.com/jsref/event_preventdefault.asp
     e.preventDefault();
     //First validate form
