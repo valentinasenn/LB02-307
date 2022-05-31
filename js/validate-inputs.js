434fc98bf02574436c776198d4c85020cff91a8e
valentinasenn <sennv@bzz.ch>
2022-05-31 18:14:55 +0200

    git init

---
+++ js/validate-inputs.js
@@ -0,0 +1,118 @@
+// read form element
+// Event listeners
+//
+// Selektoren für name, lastname, mobile, password2
+
+const form = document.getElementById('form');
+const gender = document.getElementById('gender');
+const name = document.getElementById('name');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const number = document.getElementById('number');
+const message = document.getElementById('message');
+const file = document.getElementById('file');
+const button = document.getElementById('button')
+
+
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+/* Aufgabe:
+    Validieren Sie die Mobile-Nummer ähnlich wie bei der Email mit einer
+    Regular expression (regex). Für eine geeignete regex suchen Sie
+    im Internet nach "javascript regular expression for mobile number".
+*/
+// Check phone is valid
+
+
+// Check required fields
+function checkRequired(inputArr) {
+    let isRequired = false;
+    inputArr.forEach(function(input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+            isRequired = true;
+        } else {
+            showSuccess(input);
+        }
+    });
+
+    return isRequired;
+}
+
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+}
+
+/* Aufgabe:
+        Validieren Sie, ob die beiden Passwörter übereinstimmen.
+        Falls sie nicht übereinstimmen, geben Sie (ähnlich wie in den anderen Beispielen)
+        eine Fehlermeldung dem Formular aus.
+*/
+// Check passwords match
+
+// Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function validateForm(){
+    if(!checkRequired([name, email, password])){
+        //Aufgabe: Validierung der Länge für Vorname (2 bis 20) und Nachname (2 bis 50)
+        checkLength(name, 3, 15);
+        checkLength(password, 6, 25);
+        /* Aufgabe:
+          Validierung der Telefonnumer ähnlich wie bei der Email mit einer
+          Regular expression (regex). Für eine geeignete regex suchen Sie
+          im Internet nach "javascript regular expression for mobile number"
+        * */
+        checkEmail(email);
+        /* Aufgabe:
+          Validierung Sie die beiden Passwörter, damit password
+          mit password2 übereinstimmt.
+        * */
+    }
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+    //https://www.w3schools.com/jsref/event_preventdefault.asp
+    e.preventDefault();
+    //First validate form
+    validateForm();
+});
